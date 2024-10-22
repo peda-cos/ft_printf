760b437474da6f8c2f568062682e11eb782ccf78
Pedro Da Costa E Silva Monteiro <peda-cos@c2r6p1.42sp.org.br>
2024-10-22 20:56:36 -0300

    feat: add ft_putptr_fd function to handle pointer printing

--- src/ft_utils.c
+++ src/ft_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 09:04:45 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/22 10:35:02 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/22 20:55:35 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,16 @@ int	ft_puthex_fd(unsigned int n, int uppercase, int fd)
 	if (n >= 16)
 		count += ft_puthex_fd(n / 16, uppercase, fd);
 	count += ft_putchar_fd(base[n % 16], fd);
+	return (count);
+}
+
+int	ft_putptr_fd(unsigned long ptr, int fd)
+{
+	int	count;
+
+	count = 0;
+	if (ptr >= 16)
+		count += ft_putptr_fd(ptr / 16, fd);
+	count += ft_putchar_fd("0123456789abcdef"[ptr % 16], fd);
 	return (count);
 }
