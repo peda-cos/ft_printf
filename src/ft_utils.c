982a0170e92abfa9c3da53b531136fcc816117be
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-10-22 10:35:04 -0300

    feat: add ft_puthex_fd function to ft_utils.c

--- src/ft_utils.c
+++ src/ft_utils.c
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 09:04:45 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/22 10:25:27 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/22 10:35:02 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,21 @@ int	ft_putunbr_fd(unsigned int n, int fd)
 	if (n >= 10)
 		count += ft_putunbr_fd(n / 10, fd);
 	count += ft_putchar_fd((n % 10) + '0', fd);
+	return (count);
+}
+
+int	ft_puthex_fd(unsigned int n, int uppercase, int fd)
+{
+	int		count;
+	char	*base;
+
+	count = 0;
+	if (uppercase)
+		base = "0123456789ABCDEF";
+	else
+		base = "0123456789abcdef";
+	if (n >= 16)
+		count += ft_puthex_fd(n / 16, uppercase, fd);
+	count += ft_putchar_fd(base[n % 16], fd);
 	return (count);
 }
