ffc59fb6a7fd040bfb58063d463e6a33e5e6cc72
Pedro Da Costa E Silva Monteiro <peda-cos@c3r13p3.42sp.org.br>
2024-10-22 23:23:03 -0300

    refactor: reorganize project structure for printft.h

--- includes/printft.h
+++ includes/printft.h
@@ -6,7 +6,7 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 20:48:14 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/22 23:06:47 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/22 23:22:30 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 
 #include <stdarg.h>
 
-int	ft_printf(const char *format, ...);
-int	ft_process_format(const char *format, va_list args, int *i);
 int	ft_putchar_fd(char c, int fd);
 int	ft_putstr_fd(char *s, int fd);
 int	ft_putnbr_fd(int n, int fd);
 int	ft_putunbr_fd(unsigned int n, int fd);
 int	ft_puthex_fd(unsigned int n, int uppercase, int fd);
 int	ft_putptr_fd(unsigned long ptr, int fd);
+int	ft_process_format(const char *format, va_list args, int *i);
+int	ft_printf(const char *format, ...);
 
 #endif
