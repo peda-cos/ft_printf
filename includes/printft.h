bafcbf61c243540a7446ed41778ef31ce4739210
Pedro Da Costa E Silva Monteiro <peda-cos@c2r6p1.42sp.org.br>
2024-10-22 21:03:59 -0300

    feat: add printft.h header with utility function prototypes

---
+++ includes/printft.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printft.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 20:48:14 by peda-cos          #+#    #+#             */
+/*   Updated: 2024/10/22 20:50:53 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTFT_H
+# define PRINTFT_H
+
+int	ft_putunbr_fd(unsigned int n, int fd);
+int	ft_puthex_fd(unsigned int n, int uppercase, int fd);
+
+#endif
