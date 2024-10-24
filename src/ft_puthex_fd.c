9bb94ada5587a75a3d0dc56f5ff49e9f88340932
Pedro da Costa e Silva Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-10-24 00:54:40 -0300

    refactor: split ft_puthex_fd into two functions for cleaner code

--- src/ft_puthex_fd.c
+++ src/ft_puthex_fd.c
@@ -6,24 +6,27 @@
 /*   By: peda-cos <peda-cos@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 09:04:45 by peda-cos          #+#    #+#             */
-/*   Updated: 2024/10/22 23:09:55 by peda-cos         ###   ########.fr       */
+/*   Updated: 2024/10/24 00:54:16 by peda-cos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/printft.h"
 
-int	ft_puthex_fd(unsigned int n, int uppercase, int fd)
+static int	ft_puthex(unsigned int n, const char *base, int fd)
 {
-	int		count;
-	char	*base;
+	int	count;
 
 	count = 0;
-	if (uppercase)
-		base = "0123456789ABCDEF";
-	else
-		base = "0123456789abcdef";
 	if (n >= 16)
-		count += ft_puthex_fd(n / 16, uppercase, fd);
+		count += ft_puthex(n / 16, base, fd);
 	count += ft_putchar_fd(base[n % 16], fd);
 	return (count);
+}
+
+int	ft_puthex_fd(unsigned int n, int uppercase, int fd)
+{
+	if (uppercase)
+		return (ft_puthex(n, "0123456789ABCDEF", fd));
+	else
+		return (ft_puthex(n, "0123456789abcdef", fd));
 }
