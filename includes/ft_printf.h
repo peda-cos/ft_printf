22f0a28101cf3ea0c64b3ba293ca800b811900c0
Pedro Monteiro <83892026+peda-cos@users.noreply.github.com>
2024-11-20 13:53:00 -0300

    Update ft_printf.h

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -15,7 +15,7 @@
 
 # include <stdarg.h>
 
-void	ft_putchar_fd(char c, int fd);
+int	ft_putchar_fd(char c, int fd);
 int	ft_putstr_fd(char *s, int fd);
 int	ft_putnbr_fd(int n, int fd);
 int	ft_putunbr_fd(unsigned int n, int fd);
